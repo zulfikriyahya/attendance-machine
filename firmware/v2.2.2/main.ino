0eba6a581d889751e1f6771ae2a27242ddc629cf
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-12-29 19:00:03 +0700

    Rilis Versi 2.2.2 (Stable)

--- firmware/v2.2.2/main.ino
+++ firmware/v2.2.2/main.ino
@@ -1533,10 +1533,10 @@ void loop()
       if (sleepSeconds > 43200)
         sleepSeconds = 43200;
 
-      snprintf(messageBuffer, sizeof(messageBuffer), "%dJam %dMenit",
+      snprintf(messageBuffer, sizeof(messageBuffer), "%d Jam %d Menit",
                sleepSeconds / 3600, (sleepSeconds % 3600) / 60);
       showOLED(F("SLEEP FOR"), messageBuffer);
-      delay(60000);
+      delay(30000);
 
       display.clearDisplay();
       display.display();                           // Kirim buffer kosong
